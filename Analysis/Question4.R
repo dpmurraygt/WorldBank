1b1765f3850e9432cd6ab54cdcee6d2ed5123bb6
dpmurraygt <dmurray.ie99@gmail.com>
2016-06-13 09:30:49 -0400

    Updated Question4.R

--- Analysis/Question4.R
+++ Analysis/Question4.R
@@ -1,6 +1,11 @@
 #Plot GDP for all countries
 #color by Income Group (income.group)
 
+EducationGDPClean<-data.frame(EducationGDP$CountryCode,EducationGDP$GDPDollarsMM2012,EducationGDP$IncomeGroup)
+names(EducationGDPClean)<-c("CountryCode","GDPDollarsMM2012","IncomeGroup")
 
-p <- ggplot(data = EducationGDP,aes(reorder(CountryCode,GDPDollarsMM2012), y=GDPDollarsMM2012))
+EducationGDPClean<-EducationGDPClean[!is.na(EducationGDPClean$GDPDollarsMM2012),]
+
+p <- ggplot(data = EducationGDPClean,aes(reorder(CountryCode,GDPDollarsMM2012), y=GDPDollarsMM2012))
 p+geom_point(aes(colour=IncomeGroup))+labs(title="GDP by Country", y = "2012 GDP, $MM", x="Country Code")
+
